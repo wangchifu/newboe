03ab5fa7b5c68ea4439412cef676353c653cb174
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-11-12 19:39:26 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
+        <meta http-equiv="Content-Security-Policy" content="script-src * 'unsafe-inline' 'unsafe-eval';">
         @yield('my_meta')
         <title>@yield('title')-新雲端</title>
         <!-- Favicon-->
