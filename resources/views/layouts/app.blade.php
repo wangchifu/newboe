2a84791760fe7f2d10e5768095304f2cb4ad7d96
wangchifu <wangchifu@hdes.chc.edu.tw>
2026-01-05 11:16:45 +0800

    fix venobox home end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,13 +126,20 @@
         @endauth
         <script>
             var vb = new VenoBox({
-                selector: '.venobox',
+                selector: '.venobox',                
                 numeration: true,
                 infinigall: true,
                 //share: ['facebook', 'twitter', 'linkedin', 'pinterest', 'download'],
                 spinner: 'rotating-plane',
                 maxWidth: '100%',
-                maxHeight: '90%'
+                maxHeight: '90%',
+                onPostOpen: function(el, type, item, data){
+                // 找到 iframe 並讓它取得焦點
+                const iframe = document.querySelector('.vbox-content iframe');
+                if (iframe) {
+                    iframe.focus();
+                }
+            }
             });
         
             $(document).on('click', '.vbox-close', function() {
