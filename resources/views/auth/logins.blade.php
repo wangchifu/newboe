b0e9ee08ef6006c340e17761e06ced5c0816f422
unknown <unknown>
2026-08-06 09:24:50 +0800

    登入加提示

--- resources/views/auth/logins.blade.php
+++ resources/views/auth/logins.blade.php
@@ -16,8 +16,21 @@
             彰化縣教育雲端帳號登入
         </div>
         <div class="card-body">
-            <div class="alert alert-warning fw-bold" role="alert">
-                老師調校後，請至校務系統建立該教師資料後，無法立即登入，最晚等待一天，即可用原本帳號登入使用。
+            <div id="loginCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="alert alert-warning fw-bold mb-0" role="alert">
+                            老師調校後，請至校務系統建立該教師資料後，無法立即登入，最晚等待一天，即可用原本帳號登入使用。
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="alert alert-info fw-bold mb-0" role="alert">
+                            學校端帳號沒有任何權限<br>
+                            1.請該帳號先登入本系統，以建立系統內的帳號，再由已經有「帳號管理權」的帳號給予他權限。<br>
+                            2.功能在「學校管理/學校帳號」 找到人員後按"編輯"，再給予對應的功能權限。
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">                        
                 <div class="text-center">
